120e013b725a4f1380209d656e49f606e9409bce
Paramee <paramee.induwari@gmail.com>
2023-12-26 21:10:26 +0530

    updating component1: landing page

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { Component1Component } from "./landingpage/component1/component1.component";
 
 @Component({
-  selector: 'app-root',
-  standalone: true,
-  imports: [CommonModule, RouterOutlet],
-  templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+    selector: 'app-root',
+    standalone: true,
+    templateUrl: './app.component.html',
+    styleUrl: './app.component.scss',
+    imports: [CommonModule, RouterOutlet, Component1Component]
 })
 export class AppComponent {
   title = 'lms-group4';
--- src/app/landingpage/component1/component1.component.ts
+++ src/app/landingpage/component1/component1.component.ts
@@ -1,11 +1,13 @@
 import { Component } from '@angular/core';
+import { HeaderComponent } from "../../layout/header/header.component";
+import { FooterComponent } from "../../layout/footer/footer.component";
 
 @Component({
-  selector: 'app-component1',
-  standalone: true,
-  imports: [],
-  templateUrl: './component1.component.html',
-  styleUrl: './component1.component.scss'
+    selector: 'app-component1',
+    standalone: true,
+    templateUrl: './component1.component.html',
+    styleUrl: './component1.component.scss',
+    imports: [HeaderComponent, FooterComponent]
 })
 export class Component1Component {
 
