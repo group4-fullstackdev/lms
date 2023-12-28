b3685858b410caf7b7268fcf862d4b0baf5a1520
WH Sathnara <10818161@students.plymouth.ac.uk>
2023-12-29 00:44:08 +0530

    adding account elements and login component elements

--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
 
+
 @Component({
   selector: 'app-login',
-  standalone: true,
-  imports: [],
   templateUrl: './login.component.html',
-  styleUrl: './login.component.scss'
+  styleUrls: ['./login.component.scss'] // Use styleUrls instead of styleUrl
 })
 export class LoginComponent {
+  email: string = '';
+  password: string = '';
 
+  login() {
+    // Implement your login logic here
+    console.log('Email:', this.email);
+    console.log('Password:', this.password);
+    // Add your authentication service calls or logic
+  }
 }
