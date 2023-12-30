73fbc7ea54e80e946bfd5920a5d420563d1d8945
Paramee <paramee.induwari@gmail.com>
2023-12-30 21:57:32 +0530

    moving submission elements to another component, configured form validations of that,adding staff submission elements

--- src/app/pages/submissions/submissions.component.ts
+++ src/app/pages/submissions/submissions.component.ts
@@ -1,12 +1,42 @@
+import { HttpClient } from '@angular/common/http';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-submissions',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './submissions.component.html',
   styleUrl: './submissions.component.scss'
 })
 export class SubmissionsComponent {
+
+  fileInput: any;
+  fileName: string = '';
+
+  constructor(private http: HttpClient) {}
+
+  onFileSelected(event: any) {
+    this.fileInput = event.target;
+    this.fileName = this.fileInput.files[0].name;
+  }
+
+  uploadFile() {
+    const formData = new FormData();
+    formData.append('file', this.fileInput.files[0]);
+    formData.append('fileName', this.fileName);
+
+    this.http.post('/', formData)
+      .subscribe(response => {
+        console.log('File uploaded successfully:', response);
+        // Handle response from the backend if needed
+      });
+    }
+
+    resetForm() {
+      // Reset file input and fileName
+      this.fileInput.value = null;
+      this.fileName = '';
+    }
 
 }
