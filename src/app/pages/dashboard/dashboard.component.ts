a0c17091bb395144b2a982b1a0326f041b8365c7
Paramee <paramee.induwari@gmail.com>
2024-01-05 23:20:54 +0530

    updating dashboard

--- src/app/pages/dashboard/dashboard.component.ts
+++ src/app/pages/dashboard/dashboard.component.ts
@@ -1,14 +1,20 @@
 import { Component } from '@angular/core';
 import { MaindashboardpropsComponent } from '../../subcomponents/maindashboardprops/maindashboardprops.component';
 import { RouterOutlet , Routes, RouterModule , RouterLinkActive , RouterLink} from '@angular/router';
+import { LoginService } from '../login/login.service';
 
 @Component({
   selector: 'app-dashboard',
   standalone: true,
   imports: [ MaindashboardpropsComponent , RouterLink , RouterLinkActive , RouterModule],
+  providers:[LoginService],
   templateUrl: './dashboard.component.html',
   styleUrl: './dashboard.component.scss'
 })
 export class DashboardComponent {
+
+  constructor(
+    public loginService: LoginService
+  ) {}
 
 }
